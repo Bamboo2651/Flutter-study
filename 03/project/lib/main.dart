a48dec5ca643ffe548e85b470595b4685648c8a5
tkhy2651 <tah331925@gmail.com>
2026-03-16 22:16:53 +0900

    03 end

--- 03/project/lib/main.dart
+++ 03/project/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const b = "ばなな";
+  const b = "バナナ";
   const t = Text(b);
   const c = Center(child: t);
   const s = Scaffold(body: c);
   const a = MaterialApp(home: s);
   runApp(a);
 }
+
+// こんな書き方もある
+// void main() {
+//   const a = MaterialApp(
+//     home: Scaffold(body: Center(child: Text("バナナ"))),
+//   );
+//   runApp(a);
+// }
