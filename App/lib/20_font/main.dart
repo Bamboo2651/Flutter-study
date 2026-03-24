674694ca1cdfae8dd75d0d4324e95779d2af0906
tkhy2651 <tah331925@gmail.com>
2026-03-24 18:20:33 +0900

    20 up

---
+++ App/lib/20_font/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const a = MyApp();
+  runApp(a);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('フラッター'),
+      ),
+    );
+  }
+}
