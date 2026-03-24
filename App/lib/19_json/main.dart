de60d35df873574b262fb395385a5409da35a65c
tkhy2651 <tah331925@gmail.com>
2026-03-24 17:59:55 +0900

    19 up

--- App/lib/19_json/main.dart
+++ App/lib/19_json/main.dart
@@ -1,5 +1,21 @@
-void main() {}
-void test1() async {}
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:project/19_json/vegetable.dart';
+
+void main() {
+  test1();
+}
+
+void test1() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final json = await rootBundle.loadString('stub/level1.json');
+  final map = jsonDecode(json);
+  final data = Vegetable.fromJson(map);
+  debugPrint('データの中身は $data');
+}
+
 void test2() async {}
 void test3() async {}
 void test4() async {}
